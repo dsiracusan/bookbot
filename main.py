7b6550c152afa937695bb424886cb39f0fd01899
dsiracusan <dsiracusan@gmail.com>
2025-01-09 17:36:07 -0800

    update bookbot to read amount of letters in a given book

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+print("---------------Bookbot Report---------------")
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-
-
+    print(f"{num_words} words found in this book!")
+    
+    get_char_count(text)
+    
+    print("---------------Report End---------------")
 def get_num_words(text):
     words = text.split()
     return len(words)
@@ -13,6 +18,18 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_char_count(text):
+    char_count={}
+    for char in text.lower():
+        if char.isalpha():        
+            if char in char_count:
+                char_count[char] +=1
+            else:
+                char_count[char]=1
+    print("Character Count:")
+    for char in sorted(char_count):    
+        print(f"The'{char}' was found {char_count[char]} times")
 
 
 main()
